a8c80c060a5713e29fa77d9f5ed8a4308cd26569
Skander <skanderchebil1@gmail.com>
2022-04-22 15:46:49 +0100

    added sound on click

--- src/Components/MainLogic/MainLogic.jsx
+++ src/Components/MainLogic/MainLogic.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useReducer, useState } from "react";
+import { Link } from "react-router-dom";
 import rockSvg from "../../Assets/icon-rock.svg";
 import paperSvg from "../../Assets/icon-paper.svg";
 import scissorsSvg from "../../Assets/icon-scissors.svg";
 import "./MainLogic.scss";
 import "./MainLogicMediaQ.scss";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import Sound from "../../Assets/Sound/sfx-pop.mp3";
 
 function MainLogic({ ACTIONS }) {
   const [newUserSelected, setnewUserSelected] = useState(0);
   const [state, dispatch] = useReducer(reduF, ACTIONS);
+  const audio = new Audio(Sound);
 
   function reduF(state, action) {
     switch (action.type) {
@@ -22,7 +24,6 @@ function MainLogic({ ACTIONS }) {
       case ACTIONS.PAPER:
         ACTIONS.userSelected = ACTIONS.PAPER;
         setnewUserSelected((prev) => prev + 1); //dummy var to update the selected state on the dom
-
         break;
 
       case ACTIONS.SCISSORS:
@@ -35,10 +36,23 @@ function MainLogic({ ACTIONS }) {
         break;
     }
   }
+  function Rock() {
+    dispatch({ type: ACTIONS.ROCK });
+    audio.play();
+  }
+  function Paper() {
+    dispatch({ type: ACTIONS.PAPER });
+    audio.play();
+  }
+  function Scissors() {
+    dispatch({ type: ACTIONS.SCISSORS });
+    audio.play();
+  }
 
   return (
     <>
       <div className='mainGame'>
+        <button onClick={() => audio.play()}>aaaaaa</button>;
         <div className='Proceed'>
           <h1>{ACTIONS.userSelected.toUpperCase()}</h1>
           <Link style={{ textDecoration: "none" }} to='/Game'>
@@ -49,26 +63,17 @@ function MainLogic({ ACTIONS }) {
         </div>
         <div className='mainGameFlexCont'>
           <div>
-            <div
-              className='rockPaperScissor rock'
-              onClick={() => dispatch({ type: ACTIONS.ROCK })}
-            >
+            <div className='rockPaperScissor rock' onClick={Rock}>
               <img id='rockSVG' src={rockSvg} alt='SVG HERE' />
             </div>
           </div>
 
           <span className='line'></span>
 
-          <div
-            className='rockPaperScissor paper'
-            onClick={() => dispatch({ type: ACTIONS.PAPER })}
-          >
+          <div className='rockPaperScissor paper' onClick={Paper}>
             <img src={paperSvg} alt='SVG HERE' />
           </div>
-          <div
-            className='rockPaperScissor scissors'
-            onClick={() => dispatch({ type: ACTIONS.SCISSORS })}
-          >
+          <div className='rockPaperScissor scissors' onClick={Scissors}>
             <img src={scissorsSvg} alt='SVG HERE' />
           </div>
         </div>
